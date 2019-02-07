d485c9f3a86ce939700260800576c0317b1365c2
MarishaHoza <marisha.hoza@gmail.com>
2019-02-06 23:19:17 -0800

    mostly breaking stuff

--- src/App.js
+++ src/App.js
@@ -3,80 +3,105 @@ import "./App.css";
 import BarChart from "./visualizations/BarChart";
 import RadialChart from "./visualizations/RadialChart";
 import Chart from "./visualizations/Chart";
+import bibleData from './data/bibleData.json'
+import gitaData from './data/gitaData.json'
 
 class App extends Component {
   state = {
-    temps: {},
-    city: "sf", // city whose temperatures to show
+    words: {},
+    source: "bible", // city whose temperatures to show
     range: [] // time range set by the brush
   };
 
-  componentDidMount() {
-    Promise.all([
-      fetch(`${process.env.PUBLIC_URL || ""}/sf.json`),
-      fetch(`${process.env.PUBLIC_URL || ""}/ny.json`)
-    ])
-      .then(responses => Promise.all(responses.map(resp => resp.json())))
-      .then(([sf, ny]) => {
-        sf.forEach(day => (day.date = new Date(day.date)));
-        ny.forEach(day => (day.date = new Date(day.date)));
 
-        this.setState({ temps: { sf, ny } });
-      });
-  }
 
-  updateCity = e => {
-    this.setState({ city: e.target.value });
-  };
+  // componentDidMount() {
+  //   Promise.all([
+  //     fetch(`${process.env.PUBLIC_URL || ""}/bibleData.json`),
+  //     fetch(`${process.env.PUBLIC_URL || ""}/gitaData.json`)
+  //   ])
+  //     .then(responses => Promise.all(responses.map(resp => resp.json())))
+  //     .then(([bible, gita]) => {
+  //       createNodes = (bible, gita) => {
+  //         var maxAmount = d3.max(bible, gita, function (d) { return d +d.frequency})
+  //         var radiusScale = d3.scalePow()
+  //           .exponet(0.5)
+  //           .range([2, 85])
+  //           .domain([0, maxAmount]);
+  //
+  //         var myNodes = [bible, gita].map(function (d) {
+  //           return {
+  //             id: d.id,
+  //             radius: radiusScale(+d.frequency),
+  //             value: +d.frequency,
+  //             name: d.words,
+  //             x: Math.random()*900,
+  //             y: Math.random()* 800
+  //           }
+  //         });
+  //         myNodes.sort(function (a, b) { return b.value - a.value });
+  //         return myNodes
+  //       };
+  //       // bible.forEach(word => (word.weighted = new Number(word.weighted)));
+  //       // gita.forEach(word => (word.weighted = new Number(word.weighted)));
+  //
+  //       this.setState({ words: { bible, gita } });
+  //     });
+  // }
+  //
+  // updateCity = e => {
+  //   this.setState({ source: e.target.value });
+  // };
+  //
+  // updateRange = range => {
+  //   this.setState({ range });
+  // };
 
-  updateRange = range => {
-    this.setState({ range });
-  };
+
+
+
+
+
+
 
   render() {
-    const data = this.state.temps[this.state.city];
-
-    return (
-      <div className="App">
-        <h1>
-          2017 Temperatures for
-          <select name="city" onChange={this.updateCity}>
-            {[
-              { label: "San Francisco", value: "sf" },
-              { label: "New York", value: "ny" }
-              // {label: 'Amsterdam', value: 'am'},
-            ].map(option => {
-              return (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              );
-            })}
-          </select>
-        </h1>
-        <p>
-          *warning: these are <em>not</em> meant to be good examples of data
-          visualizations,<br />
-          but just to show the possibility of using D3 and React*
-        </p>
-        <BarChart
-          data={data}
-          range={this.state.range}
-          updateRange={this.updateRange}
-        />
-        <RadialChart
-          data={data}
-          range={this.state.range} />
-        <Chart data={data} />
-
-        <p>
-          (Weather data from{" "}
-          <a href="wunderground.com" target="_new">
-            wunderground.com
-          </a>)
-        </p>
+    return(
+      <div>
+        stuff
+        <p>{bibleData.the.frequency} <br/> {gitaData.the.frequency}</p>
       </div>
-    );
+    )
+    // const data = this.state.words[this.state.source];
+    //
+    // return (
+    //   <div className="App">
+    //     <h1>
+    //       2017 Temperatures for
+    //       <select name="city" onChange={this.updateCity}>
+    //         {[
+    //           { label: "Bible", value: "bible" },
+    //           { label: "Bhagivad Gita", value: "gita" }
+    //           // {label: 'Amsterdam', value: 'am'},
+    //         ].map(option => {
+    //           return (
+    //             <option key={option.value} value={option.value}>
+    //               {option.label}
+    //             </option>
+    //           );
+    //         })}
+    //       </select>
+    //     </h1>
+    //
+    //
+    //
+    //     <BarChart
+    //       data={data}
+    //       range={this.state.range}
+    //       updateRange={this.updateRange}
+    //     />
+    //
+    //   </div>
+    // );
   }
 }
 
